cdcdeab3e073bc8e4ee805a84220c5bf022aee2c
= <=>
2024-03-28 20:05:10 +0500

    change customer authentication

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
-import './App.css';
 import Form from './Components/Form';
 function App() {
-  const [activeTab,setActiveTab]=useState("Home")
   return (
     <div className="App">
       <Form />
