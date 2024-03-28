c0b9e9c0c6d1941019eae08b3c885698ded7a145
= <=>
2024-03-28 18:25:36 +0500

    init

---
+++ src/Components/Header.js
@@ -0,0 +1,41 @@
+import React, { useEffect, useState } from 'react'
+import "./Header.css"
+import { Link, useLocation } from 'react-router-dom';
+
+const Header = () => {
+  const [activeTab, setActiveTab] = useState("Home");
+  const location = useLocation();
+
+  useEffect(() => {
+    if (location.pathname === "/") {
+      setActiveTab("Home")
+    } else if (location.pathname === "/add") {
+      setActiveTab("AddContact");
+    } else if (location.pathname === "/about") {
+      setActiveTab("About")
+    }
+  }, [location])
+  return (
+    <div className='header'>
+      <p className='logo'>Contact App</p>
+      <div className='header-right'>
+        <Link to="/" >
+          <p className={` ${activeTab === "Home" ? "active" : ""}`}
+            onClick={() => setActiveTab("Home")}>
+           <b className='tab1' style={{marginLeft:"10px",backgroundColor:"green",padding:"10px",borderRadius:"15px"}}>Home</b> 
+          </p>
+        </Link>
+        <Link to="/about" >
+          <p className={`${activeTab === "About" ? "active" : ""}`}
+            onClick={() => setActiveTab("About")}>
+            <b style={{marginLeft:"10px",backgroundColor:"green",padding:"10px",borderRadius:"15px"}}>About</b>
+          </p>
+        </Link>
+      </div>
+    </div>
+  )
+}
+
+export default Header
+
+
---
+++ src/pages/About.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div style={{marginTop:"150px"}}>
+      <h2 style={{textAlign:"center"}}>This is react Contant management system application with
+        routing using Firebase
+      </h2>
+    </div>
+  )
+}
+
+export default About
