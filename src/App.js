cb9820ab0973817c5c90a33f14d0b0ec34cf6dd4
IsrarThalvi <israrthalvi@gmail.com>
2024-04-17 12:21:59 +0500

    delete extra files

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import './App.css';
 import { Grid } from '@mui/material';
-import Name from './components/Name';
-import Signin from './components/Signin';
-import UserInterface from './components/UserInterface';
-import Customerinterface from './components/Customerinterface';
-import Table from './components/MuiTable';
-import CustomModel from './components/CustomModel';
+// import Name from './components/Name';
+// import Signin from './components/Signin';
+import UserInterface from './Components/UserInterface';
+// import Customerinterface from './components/Customerinterface';
+// import Table from './components/MuiTable';
+// import CustomModel from './components/CustomModel';
 
 
 
